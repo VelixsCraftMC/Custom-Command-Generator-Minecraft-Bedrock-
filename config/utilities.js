f4cb3b28bc3561d5536a18cac3996e641162343c
VelixsCraftMCYT <muhammadilyas030906@gmail.com>
2026-02-28 17:35:04 +0700

    Update utilities.js

--- config/utilities.js
+++ config/utilities.js
@@ -3,7 +3,7 @@ const UTILS_CONFIG = [{
     type: "Alat",
     title: "Custom Command Generator (Bedrock Edition)",
     desc: "Mendukung Semua versi terbaru 1.21.130+",
-    thumb: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/76/Impulse_Command_Block.gif/revision/latest?cb=20191017044126",
+    thumb: "command.webp",
     internalPost: "Custom Command Generator",
     externalLink: "html/command/index.html"
 }];
